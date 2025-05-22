643ce341897f164636da746fedede8e33a899d48
gkarthikeyan335 <gkarthikeyan335@gmail.com>
2025-05-22 19:20:51 +0530

    kumar code

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -10,8 +10,9 @@ public class Sample {
 		System.out.println("Test1");
 		System.out.println("Test2");
 		System.out.println("Test3");
+		System.out.println("Test5");
+		System.out.println("Test6");
 		
-
 	}
 
 }
